15894df44ea09084850df7313670de8b1197b87a
Anthony Arribasplata <anthony@upn.pe>
2025-04-22 18:18:54 -0500

    Ejemplo con Swtich

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char estado_civil;
+
+            Console.Write("Ingrese su estado civil: ");
+            estado_civil = char.Parse(Console.ReadLine().ToLower());
+
+            switch (estado_civil)
+            {
+                case 's':
+                    Console.WriteLine("Soltero");
+                    break;
+                case 'c':
+                    Console.WriteLine("Casado");
+                    break;
+                case 'v':
+                    Console.WriteLine("Viudo");
+                    break;
+                case 'd':
+                    Console.WriteLine("Divorciado");
+                    break;
+                default:
+                    Console.WriteLine("Sin especificar");
+                    break;
+            }
+        }
+    }
+}
