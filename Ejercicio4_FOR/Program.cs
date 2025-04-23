7584bcf2828b33c22ef7d8014a10e21fc7befa3e
Anthony Arribasplata <anthony@upn.pe>
2025-04-22 19:21:50 -0500

    terminado

---
+++ Ejercicio4_FOR/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4_FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j=100; j>0 ; j--)
+            {
+                Console.WriteLine(j+". Universidad Privada del Norte");
+            }
+        }
+    }
+}
---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Cual es el valor de 'n': ");
+            n = int.Parse(Console.ReadLine());
+            int suma = 0;
+            for (int i = 1; i <=n; i++)
+            {
+                suma = suma + (i * i);
+                Console.WriteLine(i*i);
+            }
+            Console.WriteLine("La suma es: "+suma);
+        }
+    }
+}
